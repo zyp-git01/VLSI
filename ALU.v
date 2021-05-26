ee768b033f1587725b485e5c90ad9f8c21d2b669
yi_peng <935284904@qq.com>
2021-05-26 16:15:58 +0800

    2021/5/26

---
+++ ALU.v
@@ -0,0 +1,11 @@
+
+//需要判断当前角度是否大于待求角度，如果大于待求角度，则顺时针旋转。
+//如果小于待求角度，则逆时针旋转
+//因此需要有一个判断信号(d_i)
+module ALU (
+    input wire [15:0] data_in_one,
+    input wire d_i,
+    input wire [15:0] data_in_two
+);
+    
+endmodule
