2437a14477710ac6bf075234764854cb8f93d002
yi_peng <935284904@qq.com>
2021-05-26 17:13:44 +0800

    test

--- ALU_TOP.v
+++ ALU_TOP.v
@@ -3,7 +3,7 @@ module ALU_TOP (
     input signed wire [15:0] y_init,
     input signed wire [15:0] x_shift,
     input signed wire [15:0] y_shift,
-
+    input wire d_i,
     input  wire [15:0] angle,
     input wire [15:0] delta_angle
 
@@ -11,5 +11,31 @@ module ALU_TOP (
     output signed wire [15:0] y_out,
     output wire [15:0] angle_out
 );
+
+
+    ALU u_x_ALU(
+        .data_in_one(),
+        .d_i(),
+        .data_in_two(),
+        .data_out(x_out)
+    );
+
+
+    ALU u_y_ALU(
+        .data_in_one(),
+        .d_i(),
+        .data_in_two(),
+        .data_out(x_out)    
+    );
+
+
     
+    ALU u_angle_ALU(
+        .data_in_one(),
+        .d_i(),
+        .data_in_two(),
+        .data_out(x_out)
+    );
+    
+
 endmodule
